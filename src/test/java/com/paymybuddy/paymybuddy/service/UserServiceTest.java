b4e4f4471bcd3c032090eaf6b532c3b7444f63f4
Michel Jazeron <michel.jazeron@gmail.com>
2025-03-05 15:05:26 +0100

    addBuddy tests

--- src/test/java/com/paymybuddy/paymybuddy/service/UserServiceTest.java
+++ src/test/java/com/paymybuddy/paymybuddy/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.paymybuddy.paymybuddy.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
@@ -12,6 +13,8 @@ import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,9 +24,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.paymybuddy.paymybuddy.dto.BuddyConnectionDTO;
 import com.paymybuddy.paymybuddy.dto.RegisterUserDTO;
 import com.paymybuddy.paymybuddy.dto.UpdateUserDTO;
+import com.paymybuddy.paymybuddy.exception.BuddyAlreadyAddedException;
+import com.paymybuddy.paymybuddy.exception.BuddyNotFoundException;
 import com.paymybuddy.paymybuddy.exception.EmailAlreadyUsedException;
+import com.paymybuddy.paymybuddy.exception.NotFoundException;
+import com.paymybuddy.paymybuddy.exception.SelfAddException;
 import com.paymybuddy.paymybuddy.exception.UsernameAlreadyTakenException;
 import com.paymybuddy.paymybuddy.model.User;
 import com.paymybuddy.paymybuddy.repository.TransactionRepository;
@@ -51,7 +59,7 @@ public class UserServiceTest {
 
     @BeforeEach
     void setUp(){
-        user = new User(1, "rory", "rory@gmail.com", "encodedPassword", BigDecimal.valueOf(100.00), null, LocalDateTime.now());
+        user = new User(1, "rory", "rory@gmail.com", "encodedPassword", BigDecimal.valueOf(100.00), new HashSet<>(), LocalDateTime.now());
     }
 
     @Test
@@ -128,35 +136,6 @@ public class UserServiceTest {
         verify(userRepository).save(any(User.class));
     }
 
-    // @Test
-    // void updateUserUsernameAlreadyTakenTest() {
-        
-    //     when(userRepository.getById(1)).thenReturn(user);
-    //     when(userRepository.existsByUsername("existingusername")).thenReturn(true);
-
-    //     AlreadyExistsException thrown = assertThrows(AlreadyExistsException.class, () -> {
-    //         userService.updateUser(new UpdateUserDTO(1, "existingUsername", "rory@gmail.com", ""));
-    //     });
-
-    //     assertEquals("Username already taken: existingusername", thrown.getMessage());
-    //     verify(userRepository, never()).save(any(User.class));
-    // }
-
-    // @Test
-    // void updateUserEmailAlreadyUsedTest() {
-        
-    //     when(userRepository.getById(1)).thenReturn(user);
-        
-    //     when(userRepository.existsByEmail("usedemail@mail.com")).thenReturn(true);
-
-    //     AlreadyExistsException thrown = assertThrows(AlreadyExistsException.class, () -> {
-    //         userService.updateUser(new UpdateUserDTO(1, "rory", "usedEmail@mail.com", ""));
-    //     });
-
-    //     assertEquals("Username already taken: usedemail@mail.com", thrown.getMessage());
-    //     verify(userRepository, never()).save(any(User.class));
-    // }
-
     @Test
     void updateUserUsernameAlreadyTakenTest() {
         when(userRepository.getById(1)).thenReturn(user);
@@ -182,7 +161,74 @@ public class UserServiceTest {
         
         assertEquals("Email already used: usedemail@mail.com", thrown.getMessage());
 
-        verify(userRepository, never()).save(any(User.class));
+        verify(userRepository, never()).save(any(User.class));    
+    }
+
+    @Test
+    void addBuddyOkTest() {
         
+        User buddy = new User(2, "stevie", "stevie@gmail.com", "encodedPassword", BigDecimal.valueOf(100.00), new HashSet<>(), LocalDateTime.now());
+        BuddyConnectionDTO buddyConnection = new BuddyConnectionDTO("rory@example.com", "stevie@example.com");
+
+        when(userRepository.findByEmail("rory@example.com")).thenReturn(Optional.of(user));
+        when(userRepository.findByEmail("stevie@example.com")).thenReturn(Optional.of(buddy));
+        when(userRepository.save(user)).thenReturn(user);
+
+        userService.addBuddy(buddyConnection);
+
+        assertTrue(user.getBuddies().contains(buddy));
+
+        verify(userRepository, times(1)).findByEmail("rory@example.com");
+        verify(userRepository, times(1)).findByEmail("stevie@example.com");
+        verify(userRepository, times(1)).save(user);
+    }
+
+    @Test
+    void addBuddySelfAddTest() {
+        
+        BuddyConnectionDTO buddyConnection = new BuddyConnectionDTO("rory@example.com", "rory@example.com");
+
+        assertThrows(SelfAddException.class, ()-> userService.addBuddy(buddyConnection));
+
+        assertTrue(!user.getBuddies().contains(user));
+        verify(userRepository, never()).save(user);
+    }
+
+    @Test
+    void addBuddyBuddyAlreadyAddedTest() {
+        User buddy = new User(2, "stevie", "stevie@gmail.com", "encodedPassword", BigDecimal.valueOf(100.00), new HashSet<>(), LocalDateTime.now());
+        BuddyConnectionDTO buddyConnection = new BuddyConnectionDTO("rory@example.com", "stevie@gmail.com");
+
+        user.getBuddies().add(buddy);
+
+        when(userRepository.findByEmail("rory@example.com")).thenReturn(Optional.of(user));
+        when(userRepository.findByEmail("stevie@gmail.com")).thenReturn(Optional.of(buddy));
+
+        assertThrows(BuddyAlreadyAddedException.class, () -> userService.addBuddy(buddyConnection));
+
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    void addBuddyBuddyNotFoundTest() {
+        BuddyConnectionDTO buddyConnection = new BuddyConnectionDTO("rory@example.com", "notexisting@gmail.com");
+
+        when(userRepository.findByEmail("rory@example.com")).thenReturn(Optional.of(user));
+        when(userRepository.findByEmail("notexisting@gmail.com")).thenReturn(Optional.empty());
+
+        assertThrows(BuddyNotFoundException.class, () -> userService.addBuddy(buddyConnection));
+
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    void addBuddyUserNotFoundTest() {
+        BuddyConnectionDTO buddyConnection = new BuddyConnectionDTO("notexisting@example.com", "rory@gmail.com");
+
+        when(userRepository.findByEmail("notexisting@example.com")).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> userService.addBuddy(buddyConnection));
+
+        verify(userRepository, never()).save(any(User.class));
     }
 }
