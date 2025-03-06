dfbd64cf61d3f389bd169989b4f6ab53a4c03436
Michel Jazeron <michel.jazeron@gmail.com>
2025-03-06 14:07:20 +0100

    test login get

---
+++ paymybuddy_test.session.sql
@@ -0,0 +1,3 @@
+USE paymybuddy_test;
+INSERT INTO user (id, user_name, email, password, balance, date_created)
+VALUES (1, 'rory', 'rory@gmail.com', '$2a$10$Wj8wft8eK9F3CZyZ.xYZwTw2LGlJKl5Ejq5fnjFupm17Xx5PAkjr6', 0, NOW());
---
+++ src/test/java/com/paymybuddy/paymybuddy/controller/LoginControllerIT.java
@@ -0,0 +1,57 @@
+package com.paymybuddy.paymybuddy.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.paymybuddy.paymybuddy.model.User;
+import com.paymybuddy.paymybuddy.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class LoginControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    public void setup() {
+        // String encodedPassword = passwordEncoder.encode("correct_password");
+        // User user = new User(1, "rory", "rory@gmail.com", encodedPassword, BigDecimal.ZERO, new HashSet<>(), LocalDateTime.now());
+        // userRepository.save(user);
+    }
+
+    @Test
+    public void showLoginFormOkIT() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+}
